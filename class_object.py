6e97dc66e0d302a6b4cc3cba3176f83e7b0b86cf
Saurabh87Bhosale <96406597+Saurabh87Bhosale@users.noreply.github.com>
2021-12-20 11:45:34 +0530

    Add files via upload

---
+++ class_object.py
@@ -0,0 +1,14 @@
+class student():
+    def __init__(self,id,name,address):
+        self.id=id
+        self.name=name
+        self.address=address
+
+    def show(self):
+        print(self.id)
+        print(self.name)
+        print(self.address)
+obj=student(104,'saurabh','pune')
+obj.show()
+obj1=student(105,'priyanka','pune')
+obj1.show()
